c0af532837f531e1c7fa1093c883003ad1568c49
Bruno <brunocosta.1991@hotmail.com>
2022-04-21 12:27:49 -0300

    criando projeto jpa

---
+++ JPA/src/test/java/github/brunocs1991/jpa/JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package github.brunocs1991.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
