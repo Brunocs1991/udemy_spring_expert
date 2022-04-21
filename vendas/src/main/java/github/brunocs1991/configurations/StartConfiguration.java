ad80828059d81434de735cb29541e3a7f83d3087
Bruno <brunocosta.1991@hotmail.com>
2022-04-21 10:14:44 -0300

    multiplos perfil's

--- vendas/src/main/java/github/brunocs1991/configurations/StartConfiguration.java
+++ vendas/src/main/java/github/brunocs1991/configurations/StartConfiguration.java
@@ -1,9 +1,18 @@
 package github.brunocs1991.configurations;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class StartConfiguration {
 
+    @Bean
+    public CommandLineRunner executar(){
+        return  args -> {
+            System.out.println("rodando configuração de desenvolvimento");
+        };
+    }
 }
