c0140be3eaedbd09df1c3482c3b54d196aa568b7
Bruno <brunocosta.1991@hotmail.com>
2022-04-21 10:04:48 -0300

    configurações persinalizadas

--- vendas/src/main/java/github/brunocs1991/configurations/StartConfiguration.java
+++ vendas/src/main/java/github/brunocs1991/configurations/StartConfiguration.java
@@ -6,8 +6,4 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class StartConfiguration {
 
-    @Bean
-    public String applicationName(){
-        return "Sistema de vendas";
-    }
 }
--- vendas/src/main/java/github/brunocs1991/controller/HelloWorldController.java
+++ vendas/src/main/java/github/brunocs1991/controller/HelloWorldController.java
@@ -3,14 +3,14 @@ package github.brunocs1991.controller;
 import github.brunocs1991.configurations.StartConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-    @Autowired
-    @Qualifier("applicationName")
+    @Value("${application.name}")
     private String applicationName;
 
     @GetMapping("hello")
