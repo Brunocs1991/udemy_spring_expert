369314fd252fffa87180508f522f1cfc65109c60
Bruno <brunocosta.1991@hotmail.com>
2022-04-23 18:37:47 -0300

    entidade produto

--- JPA/src/main/java/github/brunocs1991/jpa/domain/entity/Produto.java
+++ JPA/src/main/java/github/brunocs1991/jpa/domain/entity/Produto.java
@@ -2,13 +2,25 @@ package github.brunocs1991.jpa.domain.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Getter
 @Setter
+@ToString
+@Entity
+@Table(name ="produto")
 public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "descricao")
     private String descricao;
+
+    @Column(name = "preco_unitario")
     private BigDecimal preco;
 }
