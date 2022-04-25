6e3e6dcec53a6fc058d77b1571dc476e7398c771
Bruno <bruno@bruno.com>
2022-04-25 16:21:57 -0300

    api errors

---
+++ api-rest-vendas/src/main/java/br/com/github/brunocs1991/apirestvendas/rest/ApiErrors.java
@@ -0,0 +1,17 @@
+package br.com.github.brunocs1991.apirestvendas.rest;
+
+import lombok.Data;
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ApiErrors {
+
+    @Getter
+    private List<String> errors;
+
+    public ApiErrors(String mensagemErro) {
+        this.errors = Arrays.asList(mensagemErro);
+    }
+}
---
+++ api-rest-vendas/src/main/java/br/com/github/brunocs1991/apirestvendas/rest/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,19 @@
+package br.com.github.brunocs1991.apirestvendas.rest.controller;
+
+import br.com.github.brunocs1991.apirestvendas.exception.RegraNegocioException;
+import br.com.github.brunocs1991.apirestvendas.rest.ApiErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(RegraNegocioException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleRegraNegocioException(RegraNegocioException ex) {
+        String mensagemErro = ex.getMessage();
+        return new ApiErrors(mensagemErro);
+    }
+}
