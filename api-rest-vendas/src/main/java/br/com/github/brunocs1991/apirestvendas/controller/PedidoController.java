ca2b27cf7aa72d7bb1ee20f61d7559980b0905da
Bruno <brunocosta.1991@hotmail.com>
2022-04-25 13:35:58 -0300

    controller and service pedido

---
+++ api-rest-vendas/src/main/java/br/com/github/brunocs1991/apirestvendas/controller/PedidoController.java
@@ -0,0 +1,16 @@
+package br.com.github.brunocs1991.apirestvendas.controller;
+
+import br.com.github.brunocs1991.apirestvendas.service.PedidoService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/pedidos")
+public class PedidoController {
+
+    private PedidoService pedidoService;
+
+    public PedidoController(PedidoService pedidoService){
+        this.pedidoService = pedidoService;
+    }
+}
---
+++ api-rest-vendas/src/main/java/br/com/github/brunocs1991/apirestvendas/service/PedidoService.java
@@ -0,0 +1,7 @@
+package br.com.github.brunocs1991.apirestvendas.service;
+
+import org.springframework.stereotype.Service;
+
+public interface PedidoService {
+
+}
