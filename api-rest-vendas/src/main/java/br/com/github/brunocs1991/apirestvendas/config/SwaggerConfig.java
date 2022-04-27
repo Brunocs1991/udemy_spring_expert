c77d20ba126e716929d4ab212a737f26f22d759c
Bruno <brunocosta.1991@hotmail.com>
2022-04-27 18:51:28 -0300

    configurando segurança

---
+++ api-rest-vendas/src/main/java/br/com/github/brunocs1991/apirestvendas/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package br.com.github.brunocs1991.apirestvendas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableWebMvc
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("br.com.github.brunocs1991.apirestvendas.rest.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(this.apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Vendas API")
+                .description("Api treinamento")
+                .version("1.0")
+                .contact(this.contact())
+                .build();
+    }
+
+    private Contact contact() {
+        return new Contact("Bruno Costa e Silva", "https://github.com/Brunocs1991", "brunocosta.1991@hotmail.com");
+    }
+}
