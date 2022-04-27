df7ab1943ed1dd6fd4c4adbd0e23393dcf3d2dba
Bruno <brunocosta.1991@hotmail.com>
2022-04-27 19:15:03 -0300

    personalizando config api

--- api-rest-vendas/src/main/java/br/com/github/brunocs1991/apirestvendas/rest/controller/ClienteController.java
+++ api-rest-vendas/src/main/java/br/com/github/brunocs1991/apirestvendas/rest/controller/ClienteController.java
@@ -2,6 +2,7 @@ package br.com.github.brunocs1991.apirestvendas.rest.controller;
 
 import br.com.github.brunocs1991.apirestvendas.domain.entity.Cliente;
 import br.com.github.brunocs1991.apirestvendas.domain.repository.ClienteRepository;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
@@ -15,17 +16,28 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/clientes")
 @RequiredArgsConstructor
+@Api("Api CLientes")
 public class ClienteController {
 
     private final ClienteRepository clienteRepository;
 
     @GetMapping("/{id}")
-    public Cliente getClienteById(@PathVariable Integer id) {
+    @ApiOperation("Obter detalhes de um cliente por id")
+    @ApiResponses({
+            @ApiResponse(code= 200, message = "Cliente encontrado"),
+            @ApiResponse(code = 404, message = "Cliente não encontrado para o id informado")
+    })
+    public Cliente getClienteById(@PathVariable @ApiParam("Id do cliente") Integer id) {
         return clienteRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation("Salva um novo Cliente")
+    @ApiResponses({
+            @ApiResponse(code= 201, message = "Cliente salvo com sucesso"),
+            @ApiResponse(code = 400, message = "Erro de validação")
+    })
     public Cliente save(@RequestBody @Valid Cliente cliente) {
         return clienteRepository.save(cliente);
     }
